e8bc7a60adaced249dd9f9beed268dcb91992733
maharjanm96 <maharjanm96@gmail.com>
2024-01-30 13:41:19 +0545

    Destructuring in function parameters

--- destructuring.js
+++ destructuring.js
@@ -16,9 +16,9 @@ console.log(colors);
 //Assign array elements to variable
 const names = ["Manish", "Gojo", "Yuji", "Megumi", "Sukuna"];
 
-const[firstName, secondName, thirdName, ...restNames ] = names;
+const [firstName, secondName, thirdName, ...restNames] = names;
 
-console.log(firstName)
+console.log(firstName);
 console.log(secondName);
 console.log(thirdName);
 console.log(restNames);
@@ -26,14 +26,32 @@ console.log(restNames);
 //Object destructuring
 
 const person1 = {
-    name:"Manish",
-    age: "22",
-    isStudent: true,
-    userName: "maharjanm",
-}
+  name: "Manish",
+  age: "22",
+  isStudent: true,
+  userName: "maharjanm",
+};
 
-const{name, age, isStudent, userName} = person1;
+const { name, age, isStudent, userName } = person1;
 console.log(name);
 console.log(age);
 console.log(isStudent);
 console.log(userName);
+
+//Destructure in Function Parameters
+
+const animeDetails = {
+  name: "Jujutsu Kaisen",
+  genere: "Action",
+  mainCharacter: "Sukuna",
+  ratings: 10,
+};
+
+const displayDetails = ({ name, genere, mainCharacter, ratings }) => {
+  console.log(`Anime name is ${name}.`);
+  console.log(`Anime genere is ${genere}.`);
+  console.log(`Anime mainCharacter is ${mainCharacter}.`);
+  console.log(`Anime ratings is ${ratings}.`);
+};
+
+displayDetails(animeDetails);
