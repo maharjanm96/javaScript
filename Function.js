63d0d753eed9a39d59bf66e4c2cab39b7842a5c3
Manish Maharjan <maharjanm96@gmail.com>
2024-05-09 16:51:02 +0545

    Fucntion in JS

---
+++ Function.js
@@ -0,0 +1,13 @@
+function addition(a, b) {
+  return a + b;
+}
+console.log("Addition is ", addition(2, 2));
+
+//Arrow Function
+const multiply = (x, y) => {
+  return x * y;
+};
+
+console.log(`Multiplication of`, multiply(4, 5));
+
+
